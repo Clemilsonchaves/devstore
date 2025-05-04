0349d593d85bdd59009489124655d6ad07e599ad
clemilsonchaves <clemilsonchaves.1992@gmail.com>
2025-05-04 05:53:35 -0300

    Implement API utility and fetch featured products in Home component

--- src/data/api.ts
+++ src/data/api.ts
@@ -1,7 +1,15 @@
-export function api() {
-    const url =
+import { env } from '@/env';
 
-    return fetch()
+export function api(path: string, init?: RequestInit) {
+    const baseUrl = env.NEXT_PUBLIC_API_URL || process.env.NEXT_PUBLIC_API_URL;
+
+    if (!baseUrl) {
+        throw new Error('NEXT_PUBLIC_API_URL is not defined in the environment variables.');
+    }
+
+    const apiPrefix = '/api';
+
+    const url = new URL(apiPrefix.concat(path), baseUrl);
+
+    return fetch(url.toString(), init);
 }
-
-api('/products')
