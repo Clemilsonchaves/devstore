ccd42bf4f2be59fb7c1034da4df0b45e67b7a8e8
clemilsonchaves <clemilsonchaves.1992@gmail.com>
2025-05-13 04:16:55 -0300

    feat: refactor featured products API and implement CartContext for cart management

--- src/app/api/products/featured/route.ts
+++ src/app/api/products/featured/route.ts
@@ -1,9 +1,8 @@
-import { api } from "@/data/api";
 import data from "../data.json";
 
 export async function GET() {
     // Simula um atraso para imitar uma chamada real de API
     await new Promise(resolve => setTimeout(resolve, 2000));
-    const response = await api('/products/featured');
-    return Response.json(response);
+    const featuredProducts = data.products.filter(product => product.featured);
+    return Response.json(featuredProducts);
 }
